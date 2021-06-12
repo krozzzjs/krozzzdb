e42598f8defdc9fb320001a8750858b9d2caafbd
KrozZz <64714487+krozzzjs@users.noreply.github.com>
2021-06-12 02:16:12 +0200

    Add files via upload

---
+++ src/methods/all.js
@@ -0,0 +1,19 @@
+module.exports = function(db, params, options) {
+  
+  // Fetch Entry
+  var stmt = db.prepare(`SELECT * FROM ${options.table} WHERE ID IS NOT NULL`);
+  let resp = [];
+  for (var row of stmt.iterate()) {
+    try {
+      let data = JSON.parse(row.json)
+      if(typeof data == 'string') data = JSON.parse(data)
+      resp.push({
+        ID: row.ID,
+        data
+      })
+    } catch (e) {}
+  }
+  
+  return resp;
+  
+}
